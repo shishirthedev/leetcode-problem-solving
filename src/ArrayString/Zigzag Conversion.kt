4634c2623868bdd26020364828aa1f69d797321e
Shishir <ahidul.islam@reddotdigitalit.com>
2024-04-09 00:38:56 +0600

    solved zigzag conversion string problem

---
+++ src/ArrayString/Zigzag Conversion.kt
@@ -0,0 +1,22 @@
+package ArrayString
+
+import java.lang.StringBuilder
+
+fun main() {
+    print(convert("PAYPALISHIRING", 3))
+}
+
+fun convert(s: String, numRows: Int): String {
+    if (numRows == 1) return s
+    val strBuilder = StringBuilder()
+    for (r in 0 until numRows) {
+        val increment = 2 * (numRows - 1)
+        for (i in r until s.length step increment) {
+            strBuilder.append(s[i])
+            if (r > 0 && r < numRows - 1 && (i + increment - 2 * r) < s.length) {
+                strBuilder.append(s[i + increment - 2 * r])
+            }
+        }
+    }
+    return strBuilder.toString()
+}
