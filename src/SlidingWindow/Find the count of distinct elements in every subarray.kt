e40dbc70056463de647b1227ea2762683e07e8f9
Shishir <ahidul.islam@reddotdigitalit.com>
2024-07-18 00:14:00 +0600

    renamed file

--- src/SlidingWindow/Find the count of distinct elements in every subarray.kt
+++ src/SlidingWindow/Find the count of distinct elements in every subarray.kt
@@ -1,7 +1,31 @@
 package SlidingWindow
 
+import ListNode
+
 fun main() {
-    val r = findDistinctCount(intArrayOf(1, 2, 2), 3)
+    // val r = findDistinctCount(intArrayOf(1, 2, 2), 3)
+    val head = ListNode(1).apply {
+        next = ListNode(-3).apply {
+            next = ListNode(2)
+        }
+    }
+    var str: ListNode? = head
+    var arr = ArrayList<Int>()
+    while (str != null) {
+        arr.add(str.`val`)
+        str = str.next
+    }
+    var sortedArr = arr.sorted()
+    var preNode = ListNode(-1)
+    sortedArr.forEach {
+        println(it)
+        preNode.next = ListNode(it)
+    }
+    var ptr = preNode.next
+    while (ptr != null){
+      //  println(ptr.`val`)
+        ptr = ptr.next
+    }
 }
 
 fun findDistinctCount(nums: IntArray, k: Int): IntArray {
@@ -27,4 +51,20 @@ fun findDistinctCount(nums: IntArray, k: Int): IntArray {
         end++
     }
     return ans.toIntArray()
+}
+
+fun sortList(head: ListNode?): ListNode? {
+    val arr = ArrayList<Int>()
+    var ptr = head
+    while (ptr?.next != null) {
+        arr.add(ptr.`val`)
+        ptr = ptr.next
+    }
+    arr.sorted()
+    for (item in arr) println(item)
+    val dummyNode = ListNode(-1)
+    for (item in arr) {
+        dummyNode.next = ListNode(item)
+    }
+    return dummyNode
 }
